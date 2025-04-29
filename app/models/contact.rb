3fbea32eb90abfcf75bcdb06a721aa22ccb3d846
santiagouribe14 <santiago.uribe@kellogg.northwestern.edu>
2025-04-29 16:25:17 +0000

    class 5 changes

---
+++ app/models/contact.rb
@@ -0,0 +1,3 @@
+class Contact < ApplicationRecord
+    # talk to the contacts table
+end
--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,15 +21,41 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+new_salespeople = Salesperson.new 
+new_salespeople["first_name"]="Santiago"
+new_salespeople["last_name"]="Uribe"
+new_salespeople["email"]="santiago@gmail.com"
+
+new_salespeople.save
+
+new_salespeople = Salesperson.new 
+new_salespeople["first_name"]="Hernan"
+new_salespeople["last_name"]="Mendoza"
+new_salespeople["email"]="hernan@gmail.com"
+
+new_salespeople.save
+
 # 3. write code to display how many salespeople rows are in the database
+
+no_of_salespeople = Salesperson.all.count
+puts "Salespeople: #{no_of_salespeople}"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
 
+hernan = Salesperson.find_by({"first_name" => "Hernan"})
+hernan["email"]= "hernan.mendoza@gmail.com"
+
+hernan.save
+
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+
+for salesperson in Salesperson.all
+    puts "#{salesperson["first_name"]} #{salesperson["last_name"]}"
+end
 
 # ---------------------------------
 # Salespeople: 2
